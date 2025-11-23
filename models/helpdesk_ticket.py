87d3517878ea3ee3fea17acb0fb992541d2dd092
Crystian Vinueza <crystianvinueza@MacBook-de-Crystian.local>
2025-11-22 21:36:38 -0500

    Add approved_cost_usd field

--- models/helpdesk_ticket.py
+++ models/helpdesk_ticket.py
@@ -3,5 +3,6 @@ from odoo import models, fields
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    hours_estimated = fields.Float(string="Horas Estimadas")
-    hours_approved = fields.Float(string="Horas Aprobadas")
+    hours_estimated = fields.Float(string="Estimated Time (Hours)")
+    hours_approved = fields.Float(string="Approved Time (Hours)")
+    approved_cost_usd = fields.Float(string="Approved Cost (USD)")
